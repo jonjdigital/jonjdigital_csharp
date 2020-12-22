c2992e7e38bb5308032014c6ac35227d413e79b8
Jon James <jon.james@jonjdigital.com>
2020-12-22 16:15:30 +0000

    init c#

---
+++ JonJDigital_Bot_Proj/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace JonJDigital_Bot_Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+
+            string prefix = "j";
+            
+            DotNetEnv.Env.Load("../../../.env");
+            string token = DotNetEnv.Env.GetString("TOKEN");
+            
+            var Discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            Discord.MessageCreated += async (s, e) =>
+            {
+                PublicCmd test1 = new PublicCmd();
+
+                //ignore bots own messages
+                if(e.Message.Author.IsBot) return;
+                
+                //declare jons account IDs
+                var jonjdigital = 229244232064434177;
+                var jonjdigital_test = 691346983494877216;
+
+                // test1.helloWorld(e.Message);
+                
+                //get basic message details
+                string msg = e.Message.Content;
+                int author = (int) e.Message.Author.Id;
+
+                var response = test1.levelUp(e.Message);
+                if (response != "")
+                {
+                    e.Message.RespondAsync(test1.levelUp(e.Message));
+                }
+
+                //tester msg response
+                /*if (msg.ToLower().StartsWith(prefix + "hello"))
+                {
+                    await e.Message.RespondAsync(("Hello <@" + e.Message.Author.Id) + ">!");
+                }*/
+                
+                //public commands
+                if (msg.ToLower() == prefix + "ping")
+                {
+                    if (author == jonjdigital || author == jonjdigital_test)
+                    {
+                        // PublicCmd cmd = new PublicCmd();
+                        // Console.WriteLine(test1.ping());
+                        e.Message.RespondAsync(test1.ping());
+                        //await e.Message.RespondAsync("Hello <@" + e.Message.Author.Id + ">!");
+                    }else if (author != jonjdigital && author != jonjdigital_test)
+                    {
+                        e.Message.RespondAsync("Sorry <@" + e.Message.Author.Id + ">, but this is Developer only command!");
+
+                    }
+
+                    // Console.WriteLine();
+                }
+
+            };
+            
+            await Discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+    }
+}
---
+++ JonJDigital_Bot_Proj/PublicCmd.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using DSharpPlus.Entities;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace JonJDigital_Bot_Proj
+{
+    public class PublicCmd
+    {
+        public PublicCmd()
+        {
+        }
+
+        public string helloWorld(DiscordMessage msg)
+        {
+            return "H";
+        }
+        public string ping()
+        {
+            // DotNetEnv.Env.Load("../../../.env");
+            string conStr = DotNetEnv.Env.GetString("MYSQL_CON");
+
+            var con = new MySqlConnection(conStr);
+            con.Open();
+            string version = con.ServerVersion;
+            con.Close();
+            return "MySQL version : "+version;
+        }
+
+        public string levelUp(DiscordMessage msg)
+        {
+            var guildId = msg.Channel.Guild.Id;
+            var author= msg.Author;
+            double exp1 = msg.Content.Length / 2;
+            var experience = Math.Ceiling(exp1);
+            
+            string conStr = DotNetEnv.Env.GetString("MYSQL_CON");
+
+            var con = new MySqlConnection(conStr);
+            con.Open();
+
+            var stm = "select * from levels where user_id=" + author.Id + " and guild_id=" + guildId;
+            var cmd = new MySqlCommand(stm, con);
+            
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            
+            if (rdr.Read())
+            {
+                var user = rdr.GetInt64(0);
+                var guild = rdr.GetInt64(1);
+                var exp = rdr.GetInt64(2);
+                var level = rdr.GetInt64(3);
+
+                var levelBoundary = level * 100;
+                var newXp = exp + experience;
+                if ( newXp >= levelBoundary)
+                {
+                    level++;
+                    rdr.Close();
+                    var sql1 = $"update levels set experience = {newXp}, level = {level} where guild_id = {guild} and user_id = {user}";
+                    var cmd11 = new MySqlCommand(sql1, con);
+                    cmd11.ExecuteNonQuery();
+                    con.Close();
+                    return $"Congrats {author.Username} you have levelled up to level {level}!!";
+                }
+                
+                rdr.Close();
+                var sql = $"update levels set experience = {newXp}, level = {level} where guild_id = {guild} and user_id = {user}";
+                var cmd1 = new MySqlCommand(sql, con);
+                cmd1.ExecuteNonQuery();
+                con.Close();
+                // Console.WriteLine("row inserted");
+                // Console.WriteLine($"User: {user}, Guild: {guild}, Experience: {exp}, Level: {level}");
+                // Console.WriteLine("{0}", rdr.GetInt64(0));
+            }
+            else
+            {
+                rdr.Close();
+                var sql = $"INSERT INTO levels(user_id, guild_id, experience, level) VALUES({author.Id}, {guildId}, {experience}, 0)";
+                var cmd1 = new MySqlCommand(sql, con);
+                cmd1.ExecuteNonQuery();
+                con.Close();
+                // Console.WriteLine("row inserted");
+                // return "Gained: " + experience + " xp";
+            }
+
+            return "";
+            // Console.WriteLine(result);
+        }
+    }
+    
+}
